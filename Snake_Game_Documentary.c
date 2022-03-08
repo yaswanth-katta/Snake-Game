efbf8690533ba80e58cb633a6785268b98448b35
yaswanth katta <44583720+yaswanth-katta@users.noreply.github.com>
2022-03-09 02:08:49 +0530

    Update Snake_Game_Documentary.c
    update documentary for better understanding

--- Snake_Game_Documentary.c
+++ Snake_Game_Documentary.c
@@ -1,15 +1,15 @@
                .....Documentary for the Snake game.....
 
-  * IN snake game, how  the methods are working is...
-   -The whole snake game can be divide into some parts(methods) they are:
+  * Here is functions working is explained below.
+   -The whole snake game can be divide into some functions(parts) they are:
 
     1.Instructions(): 
        *In this method, to gave the instructions for operate the snake that is
-         -Use arrow keys to move the snake
-         -We provide the food in different places, You must be eat the food
+         -Use arrow keys to move the snake (left, right, up, down).
+         -Food is randomly generated with in the game space, snake wants to eat food, if snake eat food then the score will be incresed by 2 or 5.
          -After ate the food the length of the snake will be increase every time
-         -If you hit the edges of the box or hit your own body you will be dead
-        -You have two lives, after completing the lives snake will be dead
+         -If snake hits the edges, or hits snake body itself then the number of lives will be reduced by 1.
+        -If all lives are completed then game will terminated.
  
     2.Loading():
         *This method to load the snake game, that means to print the 177 ASCII
@@ -23,8 +23,8 @@
   
     3.Speed_level():
         *This method can control the snake speed and we can set the speed 
-           of the snake by using the Delay method(Delay is explained in below)
-         - There are three speed level (1, 2, 3).
+           of the snake by using the Delay method(Delay function is explained in below)
+         - There are three speed levels (1, 2, 3).
          -If we select the '1' then snake will move normal speed.
          -else if we select the '2' then snake will move faster than normal speed.
          -else we select the '3' then snake will more faster.
@@ -37,26 +37,30 @@
 
     5.Food():
        *This method can be provide the food in random palces every time
-           - snake can ate the food every time then this function can regenerate
-             the food 
-           -and also score can increase by using the Score mentod(explained below) 
+           - By using the random() function, we are able to generate food in defferent places with in the border. 
+           -and also score can increase by using the Score() mentod(explained below) 
 
     6.Console_change():
-        * This method can be change cursore
-          -This method is very important, it is used every method through out 
-            the program
+        * This method can be change cursore one place to another place.
+          - By considering the co-ordinates (x,y) of the compiler (display), we can change the cursor.
+          -This method is very important, This function is used through out 
+            the program.
 
     7.Right():
          *This method can be used for the snake move right side
+         - If the user clik the right arrow then right() method will invoked and the snake head will point towards right side.
 
     8.Left():
          *This method can be used for the snake move left side
+         -If the user clik the left arrow then left() method will invoked and the snake head will point towards left side.
 
     9.Up():
          *This method can be used for the snake move to the top
+         -If the user clik the up arrow then up() method will invoked and the snake head will point towards up side.
 
   10.Down():
          *This method can be used for the snake move to th bottom
+         -If the user clik the down arrow then down() method will invoked and the snake head will point towards down side.
 
   11.Bend():
        *This method can indicate the snake direction
@@ -68,17 +72,17 @@
           -And the snake can be change that direction and move
 
   13.Delay():
-        *This method can be Delay the snake that means snake can move slowly
+        *This method can be Delay the snake that means snake can move slowly.
            pseudo code:
                    for(int i=0;i<100000000;i++); 
 
   14.Score():
        *This method can display the score and the numer of lives
-         -If the snake can ate the food then Score can be increse
-         -If the snake can hit the boarder then snake life can reduce(Exitgame method).
-         - Remaining lives can display in this method
+         -If the snake ate the food then Score can be increse by 2 or 5.
+         -If the snake hits the boarder then snake life can reduce by 1 (by using Exitgame method), initially snake has three lifes.
+         - Remaining lives can be displayed by using score() method.
  
   15.Exitgame():
         *This method used to exit the game
-           -If the snake lives completed then exit the game
+           -If the snake lives completed then the game will terminate.
   
